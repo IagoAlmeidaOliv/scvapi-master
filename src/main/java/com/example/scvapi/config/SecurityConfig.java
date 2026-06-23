90251e59c6d100bed5ecd34e46a3b1bf752694ce
Iago <iagoalmeidaoliveira7@gmail.com>
2026-06-23 01:21:15 -0300

    CORS

--- src/main/java/com/example/scvapi/config/SecurityConfig.java
+++ src/main/java/com/example/scvapi/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and() // ATIVA O CORS
+                .cors().and() 
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
