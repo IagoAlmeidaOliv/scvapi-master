c9107bc47d70555d081b09d296ae07586a83640e
Iago <iagoalmeidaoliveira7@gmail.com>
2026-06-23 01:02:42 -0300

    correcao axios

--- src/main/java/com/example/scvapi/api/controller/UsuarioController.java
+++ src/main/java/com/example/scvapi/api/controller/UsuarioController.java
@@ -17,7 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/api/v1/usuarios")
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin("*")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
--- src/main/java/com/example/scvapi/config/SecurityConfig.java
+++ src/main/java/com/example/scvapi/config/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -46,8 +51,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and() // ATIVA O CORS
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
                 .antMatchers("/api/v1/tutores/**", "/api/v1/animais/**", "/api/v1/consultas/**")
                 .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/v1/veterinarios/**", "/api/v1/procedimentos/**", "/api/v1/especies/**", "/api/v1/racas/**")
@@ -62,7 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
@@ -72,5 +78,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/configuration/security",
                 "/swagger-ui.html",
                 "/webjars/**");
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(false);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
